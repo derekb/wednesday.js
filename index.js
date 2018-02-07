14ff0584a87dff7666644fff68cf1f744bb2311b
derekb <derek.burkgren@gmail.com>
2018-02-06 23:22:31 -0800

    Extract texting into a function

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ var you = process.argv[4];
 
 var client = new twilio(accountSid, authToken);
 
-client.messages.create({
-    to: you,
-    from: your_dude,
-    mediaUrl: 'http://i0.kym-cdn.com/entries/icons/original/000/020/016/wednesday.jpg'    
-});
+letYourDudeKnow(you, your_dude);
 
 console.log('It is wednesday, my dudes.');
+
+function letYourDudeKnow(you, your_dude) {
+    client.messages.create({
+        to: you,
+        from: your_dude,
+        mediaUrl: 'http://i0.kym-cdn.com/entries/icons/original/000/020/016/wednesday.jpg'    
+    });
+}
