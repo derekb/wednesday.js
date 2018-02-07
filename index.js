9603bb9ea943bb9987467815b4c69c886f215022
derekb <derek.burkgren@gmail.com>
2018-02-06 22:53:38 -0800

    Text your dudes that it is Wednesday, my dude

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var twilio = require('twilio');
+
+var accountSid = process.argv[0];
+var authToken = process.argv[1];
+
+var your_dude = process.argv[3];
+var you = process.argv[4];
+
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    to: you,
+    from: your_dude,
+    mediaUrl: 'http://i0.kym-cdn.com/entries/icons/original/000/020/016/wednesday.jpg'    
+});
+
 console.log('It is wednesday, my dudes.');
